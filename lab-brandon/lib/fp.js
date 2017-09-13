67010fbd14409fa48d6029c6a90bb46df6237265
Brandon Arroyo <brandon.jarroyo@outlook.com>
2017-09-13 09:32:24 -0700

    working with Duncan dur code review

--- lab-brandon/lib/fp.js
+++ lab-brandon/lib/fp.js
@@ -1,9 +1,14 @@
 const fp = module.exports = {};
 
 fp.map = (cb, ctx) => {
+  if(!ctx.length)
+    throw new Error('expected array like object');
   return Array.prototype.map.call(ctx, cb);
 };
 
 fp.filter = (cb, ctx) =>{
   return Array.prototype.filter.call(ctx, cb);
 };
+
+fp.map((n)=> n * 2, fp.map((n) => n + 1, [1, 2, 3]));
+// fp.badMap(fp.badMap([1,2,3], (n)=> n + 1), (n) => n * 2);
