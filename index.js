5d28d62040ac4fa16b3563690ca44486f11886f8
KatherineHanson <katherinemariehanson@gmail.com>
2017-09-12 21:16:34 -0700

    removed unnecessary console.logs

--- index.js
+++ index.js
@@ -3,46 +3,4 @@
 // assign to name of file you're referring to
 const fp = require('./lib/fp.js');
 
-// console.log(fp.map((n) => n.toUpperCase(), 'hello world'));
-// console.log(fp.map());
-// console.log(fp.map((n) => n * 2, 234));
-// console.log(fp.map((n) => n.toUpperCase(), ['h','e','l','l','o']));
-// console.log(fp.map(234));
-
-// valid
-// console.log(fp.filter((n) => n.length > 6, ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present']));
-// console.log(fp.filter((n) => n > 6, [10, 12, 3, 9, 2, -9]));
-// console.log(fp.filter((n) => n === true, [true, 12, 3, 9, 2, -9]));
-// console.log(fp.filter((n) => n.length > 6, [[3,4], [7,45,3,8,9,3,23,7,5], [8,6,3,5,1], [2], ['hi','how','are','you','doing','today','?'], ['hi']]));
-
-// invalid
-// console.log(fp.filter());
-// console.log(fp.filter((n) => n.length > 6));
-// console.log(fp.filter((n) => n > 6, []));
-// let cat = fp.reduce((p, c) => p + c)
-
-
-//valid
-// console.log(fp.reduce([3,4,7], (p, c) => p + c));
-// console.log(fp.reduce(['k','a','t','h','e','r','i','n','e'], (p, c) => p + c));
-// console.log(fp.reduce(['katherine ate ',2,' cookies'], (p, c) => p + c));
-// console.log(fp.reduce(['The answer is ',true], (p, c) => p + c));
-// console.log(fp.reduce(['hi ',['katherine',' amy',' cat']], (p, c) => p + c));
-
-//invalid
-// console.log(fp.reduce());
-// console.log(fp.reduce([], (p, c) => p + c));
-// console.log(fp.reduce(453, (p, c) => p + c));
-// console.log(fp.reduce('hello', (p, c) => p + c));
-
-// valid
-// console.log(fp.slice([1,3,6,10], 1, 3));
-// console.log(fp.slice(['katherine','amy','usagi','yuri'], 1, 3));
-// console.log(fp.slice([true,false,false,false,true], 3, 5));
-// console.log(fp.slice(['hi ',['katherine',' amy',' cat'],'how are you','today'], 1, 3));
-
-// invalid
-// console.log(fp.slice());
-// console.log(fp.slice([], 1, 3));
-// console.log(fp.slice(20, 1, 3));
-// console.log(fp.slice('hi', 1, 3));
+// add console.logs when needed for testing
