3477c0d616b6956d390f0c3062f36ba3798c933c
Ron Barrantes <ronbarrantes@gmail.com>
2017-09-12 17:55:49 -0700

    finish the filter

--- lab-ron/__test__/fp.test.js
+++ lab-ron/__test__/fp.test.js
@@ -8,3 +8,12 @@ describe('#map', () => {
     expect(fp.map(n => n * n, [2, 3, 4])).toEqual([4, 9, 16]);
   });
 });
+
+describe('#filter', () => {
+  test('filter passes an argument and filters through each element', () => {
+    expect(fp.filter(n => n > 3, [1, 2, 3, 4, 5, 6, 7])).toEqual([4, 5, 6, 7]);
+    expect(fp.filter(s => s.length === 5, ['car', 'truck', 'plane', 'bike', 'boat']))
+      .toEqual(['truck', 'plane']);
+  });
+});
+
--- lab-ron/index.js
+++ lab-ron/index.js
@@ -2,4 +2,5 @@ const fp = require('./lib/fp.js');
 
 console.log(fp.map(n => n.toUpperCase(), 'hello'));
 console.log(fp.map(n => n * n, [2, 3, 4]));
-console.log (fp.filter(n => n > 3, [1,2,3,4,5,6,7]));
+console.log(fp.filter(n => n > 3, [1, 2, 3, 4, 5, 6, 7]));
+console.log(fp.filter(s => s.length === 5, ['car', 'truck', 'plane', 'bike', 'boat']));
