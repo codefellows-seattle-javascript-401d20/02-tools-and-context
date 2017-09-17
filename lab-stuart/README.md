be51f1ec1f0b1b1a02926e89315c396ea1da3306
Stuart Kershaw <stuartdkershaw@gmail.com>
2017-09-17 11:43:31 -0700

    Cleaned up comments and improved README.md

--- lab-stuart/README.md
+++ lab-stuart/README.md
@@ -1,3 +1,7 @@
 # Lab 2 | lab-stuart
 
-Contains one module 'fp' with methods 'map', 'filter', 'reduce', 'concat', 'slice'. There are tests for these in __test__. These ensure that parameters are appropriate, or return messages to indicate if not. 
+Contains one module name 'fp' with methods fp.map, fp.filter, fp.reduce, fp.concat, and fp.slice.
+
+Each method inherits from its Array prototype counterpart. Our custom equivolents can be applied to none-Array, iterable, data structures, like strings. 
+
+Tests can be found in '__test__', and ensure that parameters to each are appropriate or return useful error messages. 
--- lab-stuart/index.js
+++ lab-stuart/index.js
@@ -1,24 +1,3 @@
 'use strict';
 
 const fp = require('./lib/fp.js');
-
-// console.log(fp.map((n) => n));
-// console.log(fp.map((n) => n, ([1, 2, 3])));
-// console.log(fp.map((n) => n, 123456));
-// console.log(fp.map((n) => n, 'hello world'));
-
-// console.log(fp.filter((n) => n > 2));
-// console.log(fp.filter((n) => n > 2, ([1, 2, 3])));
-// console.log(fp.filter((n) => n > 2, 123456));
-// console.log(fp.filter((n) => n > 2, ['cat', 1, 4, 'by', 'chair']));
-
-// console.log(fp.reduce((p,n) => p + n));
-// console.log(fp.reduce((p,n) => p + n, '_start_'));
-// console.log(fp.reduce((p,n) => p + n, '_start_', '_end_'));
-// console.log(fp.reduce((p,n) => p + n, ([123]), ([456])));
-// console.log(fp.reduce((p,n) => p + n, 123, 456));
-// console.log(fp.reduce((p,n) => p + n, ['cat', 'by', 'chair'], ['dog']));
-
-// console.log(fp.concat([1, 2, 3], [4, 5, 6]));
-
-// console.log(fp.slice(1, 4, 'hello world'));
